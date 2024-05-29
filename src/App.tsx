48bbb5cd4c1ef554b2b4eb0623e869c2505cbe83
soonen <globe0707@naver.com>
2024-05-29 16:02:28 +0900

    0이 아닐때 리셋 버튼 생성

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,22 @@ function App() {
   const count = 리턴값[0];
   const setcount = 리턴값[1];
 
+let 리셋버튼;
+  if (count !== 0) {
+  리셋버튼 = <button onClick={() => setcount(0)}>○</button>;
+  }
+
+
   return (
     <div>
       <h1>{count}</h1>
-      <h2>
         <button onClick={() => setcount((현재count) => 현재count + 1)}>
           ▲
         </button>
-        <button onClick={() => setcount(0)}>
-          ○
-        </button>
+        {리셋버튼}
         <button onClick={() => setcount((현재count) => 현재count - 1)}>
           ▼
         </button>
-      </h2>
     </div>
   );
 }
