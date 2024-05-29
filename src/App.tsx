e35433c29648c87da5fb4099f3b1f562006fd2ad
soonen <globe0707@naver.com>
2024-05-29 17:01:50 +0900

    카운터 1000개 (야매)

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,29 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
-function Counter()  {
+function Counter() {
   const 리턴값 = useState(0);
   const count = 리턴값[0];
   const setcount = 리턴값[1];
 
-let 리셋버튼;
+  let 리셋버튼;
   if (count !== 0) {
-  리셋버튼 = <button onClick={() => setcount(0)}>○</button>;
+    리셋버튼 = <button onClick={() => setcount(0)}>○</button>;
   }
-
 
   return (
     <div>
       <h1>{count}</h1>
-        <button onClick={() => setcount((현재count) => 현재count + 1)}>
-          ▲
-        </button>
-        <button onClick={() => setcount((현재count) => 현재count - 1)}>
-          ▼
-        </button>
-        {리셋버튼}
+      <button onClick={() => setcount((현재count) => 현재count + 1)}>▲</button>
+      <button onClick={() => setcount((현재count) => 현재count - 1)}>▼</button>
+      {리셋버튼}
     </div>
   );
 }
 
 function App() {
-  return (
-    <div>
-    <Counter></Counter>
-    <Counter></Counter>
-    <Counter></Counter>
-    </div>
-  )
+  const array = new Array(1000).fill(Counter);
+  return array.map((Counter) => <Counter />);
 }
 
 export default App;
